7060fd09846c3ddc4e27d50c6e2fcff8aab5857c
Codebells <1347103071@qq.com>
2022-11-10 19:59:19 +0800

    pass 3A 1test 2test timeout

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,11 +3,19 @@ package kvraft
 import "6.824/labrpc"
 import "crypto/rand"
 import "math/big"
-
+import (
+	mathrand "math/rand"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	//leaderId其实是为了下次能够直接发给正确leader
+	leaderId	int
+	//如果这个leader在commit log后crash了，但是还没响应给client，client就会重发这条command给新的leader，这样就会导致这个op执行两次。
+	//而这种解决办法就是每次发送操作时附加一个唯一的序列号去为了标识操作,避免op被执行两次
+	sequenceNum	int
+	clientId	int64
 }
 
 func nrand() int64 {
@@ -21,6 +29,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()
+	ck.leaderId = mathrand.Intn(len(ck.servers))
 	return ck
 }
 
@@ -37,9 +47,34 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
+	var args GetArgs 
+	args = GetArgs{
+		Key : key ,
+		SequenceNum : ck.sequenceNum ,
+		ClientId : ck.clientId	,
+	}
+	var reply GetReply
+	serverId := ck.leaderId
+	for{
+		reply = GetReply{}
+		ck.sequenceNum++
+		ok := ck.servers[serverId].Call("KVServer.Get", &args, &reply)
+		serverId = (serverId + 1) % len(ck.servers)
+		if !ok {
+			continue
+		}
+		if reply.Err == ErrNoKey{
+			ck.leaderId = serverId
+			return ""
+		}else if reply.Err == ErrWrongLeader{
+			serverId = (serverId + 1) % len(ck.servers)
+			continue
+		}else if reply.Err == OK{
+			ck.leaderId = serverId
+			return reply.Value
+		}
+	}
 	// You will have to modify this function.
-	return ""
 }
 
 //
@@ -54,6 +89,30 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	ck.sequenceNum++
+	serverId := ck.leaderId
+	args := PutAppendArgs{
+		Key: key, 
+		Value: value, 
+		Op: op, 
+		ClientId: ck.clientId, 
+		SequenceNum: ck.sequenceNum,
+	}
+	for {
+		reply := PutAppendReply{}
+		ok := ck.servers[serverId].Call("KVServer.PutAppend", &args, &reply)
+		serverId = (serverId + 1) % len(ck.servers)
+		if !ok {
+			continue
+		}
+		if reply.Err == OK {
+			ck.leaderId = serverId
+			return
+		} else if reply.Err == ErrWrongLeader {
+			serverId = (serverId + 1) % len(ck.servers)
+			continue
+		}	
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"sync"
 	"sync/atomic"
+	"bytes"
+	"time"
 )
 
 const Debug = false
@@ -23,6 +25,12 @@ type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	SequenceNum		int
+	Key      string
+	Value    string
+	ClientId int64
+	Index    int // raft服务层传来的Index
+	OpType   string
 }
 
 type KVServer struct {
@@ -35,15 +43,74 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	seqMap    map[int64]int     //为了确保seq只执行一次	clientId / SequenceNum
+	waitChMap map[int]chan Op   //传递由下层Raft服务的appCh传过来的command	index / chan(Op)
+	kvPersist map[string]string // 存储持久化的KV键值对	K / V
+	lastIncludeIndex int
 }
 
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	_,isLeader := kv.rf.GetState()
+	if (!isLeader) || kv.killed(){
+		reply.Err = ErrWrongLeader
+		return 
+	}
+	op := Op{OpType: "Get", Key: args.Key, SequenceNum: args.SequenceNum, ClientId: args.ClientId}
+	lastIndex, _, _ := kv.rf.Start(op)
+	ch := kv.getWaitCh(lastIndex)
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.waitChMap, op.Index)
+		kv.mu.Unlock()
+	}()
+	timer := time.NewTicker(100 * time.Millisecond)
+	defer timer.Stop()
+	select {
+	case replyOp := <-ch:
+		if op.ClientId != replyOp.ClientId || op.SequenceNum != replyOp.SequenceNum {
+			reply.Err = ErrWrongLeader
+		} else {
+			reply.Err = OK
+			kv.mu.Lock()
+			reply.Value = kv.kvPersist[args.Key]
+			kv.mu.Unlock()
+			return
+		}
+	case <-timer.C:
+		reply.Err = ErrWrongLeader
+	}
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	_,isLeader := kv.rf.GetState()
+	if (!isLeader) || kv.killed(){
+		reply.Err = ErrWrongLeader
+		return 
+	}
+	op := Op{OpType: args.Op, Key: args.Key ,Value:args.Value , SequenceNum: args.SequenceNum, ClientId: args.ClientId}
+	lastIndex, _, _ := kv.rf.Start(op)
+	ch := kv.getWaitCh(lastIndex)
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.waitChMap, op.Index)
+		kv.mu.Unlock()
+	}()
+	timer := time.NewTicker(100 * time.Millisecond)
+	defer timer.Stop()
+	select {
+	case replyOp := <-ch:
+		if op.ClientId != replyOp.ClientId || op.SequenceNum != replyOp.SequenceNum {
+			reply.Err = ErrWrongLeader
+		} else {
+			reply.Err = OK
+			return
+		}
+	case <-timer.C:
+		reply.Err = ErrWrongLeader
+	}
 }
 
 //
@@ -96,6 +163,108 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	kv.seqMap = make(map[int64]int)
+	kv.kvPersist = make(map[string]string)
+	kv.waitChMap = make(map[int]chan Op)
+	kv.lastIncludeIndex = -1
 
+	// crash
+	snapshot := persister.ReadSnapshot()
+	if len(snapshot) > 0 {
+		kv.DecodeSnapShot(snapshot)
+	}
+	go kv.runMessageHandler()
 	return kv
+}
+
+func (kv *KVServer) DecodeSnapShot(snapshot []byte) {
+	if snapshot == nil || len(snapshot) < 1 {
+		return
+	}
+
+	r := bytes.NewBuffer(snapshot)
+	d := labgob.NewDecoder(r)
+
+	var kvPersist map[string]string
+	var seqMap map[int64]int
+
+	if d.Decode(&kvPersist) == nil && d.Decode(&seqMap) == nil {
+		kv.kvPersist = kvPersist
+		kv.seqMap = seqMap
+	} else {
+		DPrintf("[Server(%v)] Failed to decode snapshot", kv.me)
+
+	}
+}
+
+func (kv *KVServer) PersistSnapShot() []byte {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(kv.kvPersist)
+	e.Encode(kv.seqMap)
+	data := w.Bytes()
+	return data
+}
+
+func (kv *KVServer) runMessageHandler(){
+	for{
+		if kv.killed(){
+			return 
+		}
+		select {
+		case msg := <-kv.applyCh:
+			if msg.CommandValid{
+				if msg.CommandIndex <  kv.lastIncludeIndex{
+					return 
+				}
+				op := msg.Command.(Op)
+				if !kv.isRepeat(op.ClientId,op.SequenceNum){
+					kv.mu.Lock()
+					switch op.OpType {
+					case "Put":
+						kv.kvPersist[op.Key] = op.Value
+					case "Append":
+						kv.kvPersist[op.Key] += op.Value	
+					}
+					kv.seqMap[op.ClientId] = op.SequenceNum
+					kv.mu.Unlock()
+				}
+				if (kv.maxraftstate!=-1 && kv.maxraftstate < kv.rf.GetRaftStateSize()){
+					snapshot := kv.PersistSnapShot()
+					kv.rf.Snapshot(msg.CommandIndex,snapshot)
+				}
+				kv.getWaitCh(msg.CommandIndex) <- op
+			}
+			if msg.SnapshotValid{
+				kv.mu.Lock()
+				if kv.rf.CondInstallSnapshot(msg.SnapshotTerm, msg.SnapshotIndex, msg.Snapshot){
+					kv.DecodeSnapShot(msg.Snapshot)
+					kv.lastIncludeIndex = msg.SnapshotIndex
+				}
+				kv.mu.Unlock()
+			}
+		}
+	}
+}
+func (kv *KVServer) isRepeat(clientId int64, seqId int) bool {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	lastSeqId, exist := kv.seqMap[clientId]
+	if !exist {
+		return false
+	}
+	return seqId <= lastSeqId
+}
+func (kv *KVServer) getWaitCh(index int) chan Op{
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	ch, exist := kv.waitChMap[index]
+	if !exist {
+		kv.waitChMap[index] = make(chan Op, 1)
+		ch = kv.waitChMap[index]
+	}
+	return ch
 }
